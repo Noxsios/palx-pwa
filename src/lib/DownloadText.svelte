abf10c95a420d212671d1766362d22d4b7f0bc35
Harry Randazzo <harry.razzle.randazzo@pm.me>
2021-11-24 16:20:50 -0600

    add legacy fallback to download

--- src/lib/DownloadText.svelte
+++ src/lib/DownloadText.svelte
@@ -44,17 +44,34 @@
 
   onMount(() => {
     // @ts-ignore
-    if (!window.FileSystemWritableFileStream) {
+    if (!window.showSaveFilePicker) {
       isSupported = false;
-      title = "File System API not supported";
-      content = "Your browser does not support the File System API.";
     }
   });
 </script>
 
 {#if isSupported === false}
-  <small class="mx-2 underline text-blue-500">
-    <a href="https://caniuse.com/fileapi">File System API not supported</a></small
+  <!-- svelte-ignore missing-declaration -->
+  <a
+    href={URL.createObjectURL(new Blob([content], { type: "text/plain" }))}
+    download={fileOptions.suggestedName}
+    class="text-purple-100 mx-2 hover:underline hover:ring-4 ring-purple-400 rounded-md bg-indigo-600 p-2 transition-all"
+  >
+    <span>{title}</span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 inline-block"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+      />
+    </svg></a
   >
 {:else}
   <button
